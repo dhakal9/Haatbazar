6f7c623a8c70da3c34d7c68229d53fa7d0be0dcc
Amrit Dhakal <dhakalamrit19@gmail.com>
2022-04-04 23:12:45 +0545

    User Login page

--- market/models.py
+++ market/models.py
@@ -6,14 +6,12 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    discription = models.TextField()
-    price= models.DecimalField(max_digits=10, decimal_places=2)
-    used_time = models.IntegerField()
+    content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.title)++ ": Rs" + str(self.price)
+        return str(self.title)+ ": Rs" + str(self.price)
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
