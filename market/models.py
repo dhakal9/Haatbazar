8e87fd862806316c4f06e597ee32b852dfb465b7
Amrit Dhakal <dhakalamrit19@gmail.com>
2022-04-04 23:21:21 +0545

    added descriptions

--- market/models.py
+++ market/models.py
@@ -6,10 +6,10 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    description = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    price = models.IntegerField(default=0)
     def __str__(self):
         return str(self.title)+ ": Rs" + str(self.price)
 
